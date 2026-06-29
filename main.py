340d7d2f21c1de26c3dae8e94bbc8e37e75c881e
FO745 <fouzia.ouaj@gmail.com>
2026-06-29 16:18:57 +0200

    instruction installation

---
+++ main.py
@@ -0,0 +1,36 @@
+from groq import Groq
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+
+API_KEY = os.getenv("GROQ_API_KEY")
+
+
+client = Groq(api_key=API_KEY)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        # Set an optional system message. This sets the behavior of the
+        # assistant and can be used to provide specific instructions for
+        # how it should behave throughout the conversation.
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        # Set a user message for the assistant to respond to.
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+
+    # The language model which will generate the completion.
+    model="llama-3.3-70b-versatile"
+)
+
+# Print the completion returned by the LLM.
+print(chat_completion.choices[0].message.content)
